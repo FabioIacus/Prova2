0bc5c8223245fc51b7b9f9f5d21b5d56f57ec84c
fabio <fabio.iacus01@gmail.com>
2023-10-20 17:29:54 +0200

    commit1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    /*questo è un commento
+    proviamo
+    come se fosse codice
+     */
 }
